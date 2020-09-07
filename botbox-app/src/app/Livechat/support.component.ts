19d2b08d32edf5f813e54f56ef8e06e188415178
Elshad Suleymanli <esuleymanli2018@ada.edu.az>
2020-09-07 04:29:23 +0400

    3boards added, no completion

---
+++ botbox-app/src/app/Livechat/support.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'app-support',
+    templateUrl: './support.component.html',
+    styleUrls: ['./livechat.component.css']
+})
+export class SupportComponent {
+
+}
--- botbox-app/src/app/app.module.ts
+++ botbox-app/src/app/app.module.ts
@@ -18,26 +18,50 @@ import { LivechatComponent } from './Livechat/livechat.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from './Menu/navbar.component';
+import { DashboardComponent } from './Forms/dashboard.component';
+import { ChatboxComponent } from './Livechat/chatbox.component';
+import { SupportComponent } from './Livechat/support.component';
+import { UserdetailsComponent } from './Livechat/userdetails.component';
+import { SidebarComponent } from './Menu/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    /*  Login component starts here */
     LoginComponent,
     ForgotComponent,
     ResetComponent,
     SuccessComponent,
     ForgotSuccessComponent,
     ForgotFailComponent,
+        /*  Login component ends here */
+
+            /*  Registration component starts here */
     RegisterComponent,
+   
+    /*  Registration component ends here */
     LivechatComponent,
-    NavbarComponent
-  
+
+    /*  Menu items start*/
+    NavbarComponent,
+    SidebarComponent,
+    DashboardComponent,
+    /*  Menu items end*/
+    
+    /*livechat component registration */
+    ChatboxComponent,
+    SupportComponent,
+    UserdetailsComponent,
+    /* livechat ends here*/
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-   
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
